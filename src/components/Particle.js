9c24eb7abc38014794dd5652a8a0e0c337aab663
Andrew Tong <andrewtong0@gmail.com>
2020-06-23 03:10:23 -0700

    Drawing to Canvas, Gravity, and Bounds
    Drawing on canvas has been implemented, scaling factor required for canvas, particles are affected by gravity and remain within bounds. Particle collision does not work.

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,23 +1,37 @@
-export default function createParticle(p_width, p_height, color, p_x, p_y, canvasRef) {
+import constants from '../constants.js';
+
+export default function createParticle(pId, p_width, p_height, color, x, y, canvas_width, canvas_height, worldRef) {
+  this.particleId = pId;
   this.width = p_width;
   this.height = p_height;
-  this.x = p_x;
-  this.y = p_y;
+  this.x = x;
+  this.y = y;
+  this.canvas_width = canvas_width;
+  this.canvas_height = canvas_height;
+  this.canvasContext = worldRef.canvasElement.getContext("2d");
+  this.hasGravity = true;
 
   this.tick = () => {
-    let context = canvasRef.getContext("2d");
-
-    context.beginPath();
-    context.rect(this.x, this.y, this.width, this.height);
-    context.fillStyle = color;
-    context.fill();
+    this.canvasContext.fillStyle = color;
+    if (this.canFall()) {
+      this.y += constants.PARTICLE_FALL_SPEED;
+    }
+    this.canvasContext.fillRect(this.x, this.y, this.width, this.height);
   }
 
-  function applyGravity() {
-
+  this.canFall = () => {
+    return (
+      this.hasGravity &&
+      this.x > 0 &&
+      this.x < this.canvas_width - constants.CANVAS_BORDER_OFFEST,
+      this.y > 0 &&
+      this.y < this.canvas_height - constants.CANVAS_BORDER_OFFEST &&
+      !worldRef.isAnotherParticleAtCoordinates(x, y + constants.PARTICLE_FALL_SPEED, this.particleId)
+    );
   }
 
-  function canFall() {
-
+  this.updateCanvasDimensions = (new_width, new_height) => {
+    this.canvas_width = new_width;
+    this.canvas_height = new_height;
   }
 }
--- src/components/World.js
+++ src/components/World.js
@@ -8,50 +8,73 @@ export default class World extends React.Component {
     super(props);
 
     this.worldTick = this.worldTick.bind(this);
-    this.coordinatesWithinBounds = this.coordinatesWithinBounds.bind(this);
+    this.worldClear = this.worldClear.bind(this);
     this.addParticleToWorld = this.addParticleToWorld.bind(this);
+    this.isAnotherParticleAtCoordinates = this.isAnotherParticleAtCoordinates.bind(this);
+    this.isCoordinateCollidingWithParticle = this.isCoordinateCollidingWithParticle.bind(this);
 
     this.state = {
       worldParticles: []
     }
+    this.canvasElement = null;
   }
 
   componentDidMount() {
+    this.canvasElement = document.getElementById("worldCanvas");
     this.interval = setInterval(() => {
       this.worldTick();
     }, constants.WORLD_SPEED);
   }
 
   worldTick() {
+    this.worldClear();
     for (let i = 0; i < this.state.worldParticles.length; i++) {
       let particle = this.state.worldParticles[i];
       particle.tick();
     }
   }
 
-  coordinatesWithinBounds(xpos, ypos) {
-    const worldContainerInfo = document.getElementById("worldCanvas").getBoundingClientRect();
-    // Gets coordinates for upper left corner
-    const worldContainer_UL_X = worldContainerInfo.left;
-    const worldContainer_UL_Y = worldContainerInfo.top;
-    const worldContainer_BR_X = worldContainer_UL_X + this.state.width;
-    const worldContainer_BR_Y = worldContainer_UL_Y + this.state.height;
-    return (
-      xpos < worldContainer_BR_X - constants.PARTICLE_SIZE &&
-      xpos > worldContainer_UL_X + constants.PARTICLE_SIZE &&
-      ypos < worldContainer_BR_Y - constants.PARTICLE_SIZE &&
-      ypos > worldContainer_UL_Y + constants.PARTICLE_SIZE
-    );
+  worldClear() {
+    const canvas = this.canvasElement;
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
   }
 
   addParticleToWorld(event) {
-    let x = event.clientX;
-    let y = event.clientY;
-    const canvasRef = document.getElementById("worldCanvas");
-    let particle = new createParticle(constants.PARTICLE_SIZE, constants.PARTICLE_SIZE, "yellow", x, y, canvasRef);
+    const canvasElementRect = this.canvasElement.getBoundingClientRect();
+    const width_scaling = this.canvasElement.width / canvasElementRect.width;
+    const height_scaling = this.canvasElement.height / canvasElementRect.height;
+    const x = (event.clientX - canvasElementRect.left) * width_scaling;
+    const y = (event.clientY - canvasElementRect.top) * height_scaling;
+    let particle = new createParticle(
+      this.state.worldParticles.length,
+      constants.PARTICLE_SIZE,
+      constants.PARTICLE_SIZE,
+      constants.PARTICLE_COLOUR,
+      x,
+      y,
+      canvasElementRect.width,
+      canvasElementRect.height,
+      this
+    );
     this.setState({
       worldParticles: this.state.worldParticles.concat([particle])
     });
+  }
+
+  isAnotherParticleAtCoordinates(x, y, particleId) {
+    for (let i = 0; i < this.state.worldParticles.length; i++) {
+      const particle = this.state.worldParticles[i];
+      if (particleId !== particle.particleId && this.isCoordinateCollidingWithParticle(x, y, particle)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isCoordinateCollidingWithParticle(x, y, particle) {
+    const insideParticleX = x < particle.x && x > particle.x + constants.PARTICLE_SIZE;
+    const insideParticleY = y < particle.y && y > particle.y + constants.PARTICLE_SIZE;
+    return(insideParticleX && insideParticleY);
   }
 
   render() {
@@ -59,7 +82,8 @@ export default class World extends React.Component {
       <canvas
         id="worldCanvas"
         onClick={this.addParticleToWorld}
-        style={{width: constants.CANVAS_WIDTH, height: constants.CANVAS_HEIGHT, context: "2d"}}
+        width={constants.CANVAS_WIDTH}
+        height={constants.CANVAS_HEIGHT}
       />
     );
   }
